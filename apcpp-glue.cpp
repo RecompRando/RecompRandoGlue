d22066e40694d74938d03458ea775cd74f048fa2
LittleCube <littlecubehax@gmail.com>
2025-03-12 04:33:36 -0400

    don't read apconnect (#8)

--- apcpp-glue.cpp
+++ apcpp-glue.cpp
@@ -4,6 +4,61 @@
 #include "Archipelago.h"
 #include "apcpp-glue.h"
 
+#if 0 // For native compilation
+#  define PTR(x) x*
+#  define RDRAM_ARG
+#  define RDRAM_ARG1
+#  define PASS_RDRAM
+#  define PASS_RDRAM1
+#  define TO_PTR(type, var) var
+#  define GET_MEMBER(type, addr, member) (&addr->member)
+#  ifdef __cplusplus
+#    define NULLPTR nullptr
+#  endif
+#else
+#  define PTR(x) int32_t
+#  define RDRAM_ARG uint8_t *rdram,
+#  define RDRAM_ARG1 uint8_t *rdram
+#  define PASS_RDRAM rdram,
+#  define PASS_RDRAM1 rdram
+#  define TO_PTR(type, var) ((type*)(&rdram[(uint64_t)var - 0xFFFFFFFF80000000]))
+#  define GET_MEMBER(type, addr, member) (addr + (intptr_t)&(((type*)nullptr)->member))
+#  ifdef __cplusplus
+#    define NULLPTR (PTR(void))0
+#  endif
+#endif
+
+typedef uint64_t gpr;
+
+#define SIGNED(val) \
+    ((int64_t)(val))
+
+#define ADD32(a, b) \
+    ((gpr)(int32_t)((a) + (b)))
+
+#define SUB32(a, b) \
+    ((gpr)(int32_t)((a) - (b)))
+
+#define MEM_W(offset, reg) \
+    (*(int32_t*)(rdram + ((((reg) + (offset))) - 0xFFFFFFFF80000000)))
+
+#define MEM_H(offset, reg) \
+    (*(int16_t*)(rdram + ((((reg) + (offset)) ^ 2) - 0xFFFFFFFF80000000)))
+
+#define MEM_B(offset, reg) \
+    (*(int8_t*)(rdram + ((((reg) + (offset)) ^ 3) - 0xFFFFFFFF80000000)))
+
+#define MEM_HU(offset, reg) \
+    (*(uint16_t*)(rdram + ((((reg) + (offset)) ^ 2) - 0xFFFFFFFF80000000)))
+
+#define MEM_BU(offset, reg) \
+    (*(uint8_t*)(rdram + ((((reg) + (offset)) ^ 3) - 0xFFFFFFFF80000000)))
+
+#define SD(val, offset, reg) { \
+    *(uint32_t*)(rdram + ((((reg) + (offset) + 4)) - 0xFFFFFFFF80000000)) = (uint32_t)((gpr)(val) >> 0); \
+    *(uint32_t*)(rdram + ((((reg) + (offset) + 0)) - 0xFFFFFFFF80000000)) = (uint32_t)((gpr)(val) >> 32); \
+}
+
 #define GI_TRUE_SKULL_TOKEN GI_75
 
 #define GI_AP_PROG GI_77
@@ -159,9 +214,24 @@ int64_t last_location_sent;
 
 void syncLocation(int64_t location_id)
 {
+    if (location_id == 0)
+    {
+        return;
+    }
+    
     if (location_id == last_location_sent)
     {
         while (!AP_GetLocationIsChecked(state, location_id));
+    }
+}
+
+void getStr(uint8_t* rdram, PTR(char) ptr, std::string& outString) {
+    char c = MEM_B(0, (gpr) ptr);
+    u32 i = 0;
+    while (c != 0) {
+        outString += c;
+        i += 1;
+        c = MEM_B(i, (gpr) ptr);
     }
 }
 
@@ -171,134 +241,119 @@ extern "C"
     
     DLLEXPORT void rando_init(uint8_t* rdram, recomp_context* ctx)
     {
-        std::ifstream apconnect("apconnect.txt");
+        std::string address;
+        std::string playerName;
+        std::string password;
         
-        if (apconnect.good())
+        PTR(char) address_ptr = _arg<0, PTR(char)>(rdram, ctx);
+        PTR(char) player_name_ptr = _arg<1, PTR(char)>(rdram, ctx);
+        PTR(char) password_ptr = _arg<2, PTR(char)>(rdram, ctx);
+        
+        getStr(rdram, address_ptr, address);
+        getStr(rdram, player_name_ptr, playerName);
+        getStr(rdram, password_ptr, password);
+        
+        state = AP_New();
+        AP_Init(state, address.c_str(), "Majora's Mask Recompiled", playerName.c_str(), password.c_str());
+        //AP_Init("apsolostartinventory.json");
+        
+        AP_SetDeathLinkSupported(state, true);
+        
+        AP_Start(state);
+        
+        while (!AP_IsConnected(state))
         {
-            std::string apEnabled;
-            glueGetLine(apconnect, apEnabled);
-            
-            if (apEnabled == "true")
+            if (AP_GetConnectionStatus(state) == AP_ConnectionStatus::ConnectionRefused || AP_GetConnectionStatus(state) == AP_ConnectionStatus::NotFound)
             {
-                std::string address;
-                std::string playerName;
-                std::string password;
-                
-                glueGetLine(apconnect, address);
-                glueGetLine(apconnect, playerName);
-                glueGetLine(apconnect, password);
-                
-                state = AP_New();
-                AP_Init(state, address.c_str(), "Majora's Mask Recompiled", playerName.c_str(), password.c_str());
-                //AP_Init("apsolostartinventory.json");
-                
-                AP_SetDeathLinkSupported(state, true);
-                
-                AP_Start(state);
-                
-                while (!AP_IsConnected(state))
+                AP_Stop(state);
+                _return(ctx, false);
+                return;
+            }
+        }
+        
+        AP_QueueLocationScoutsAll(state);
+        
+        if (AP_GetSlotDataInt(state, "skullsanity") == 2)
+        {
+            for (int i = 0x00; i <= 0x1E; ++i)
+            {
+                if (i == 0x03)
                 {
-                    if (AP_GetConnectionStatus(state) == AP_ConnectionStatus::ConnectionRefused || AP_GetConnectionStatus(state) == AP_ConnectionStatus::NotFound)
-                    {
-                        AP_Stop(state);
-                        apconnect.close();
-                        *((u8*) 0x0) = 0x0;  // screw no ui just crash everything
-                        return;
-                    }
+                    continue;
                 }
                 
-                AP_QueueLocationScoutsAll(state);
-                
-                if (AP_GetSlotDataInt(state, "skullsanity") == 2)
+                int64_t location_id = 0x3469420062700 | i;
+                AP_RemoveQueuedLocationScout(state, location_id);
+            }
+            for (int i = 0x01; i <= 0x1E; ++i)
+            {
+                int64_t location_id = 0x3469420062800 | i;
+                AP_RemoveQueuedLocationScout(state, location_id);
+            }
+        }
+        
+        for (int64_t i = AP_GetSlotDataInt(state, "starting_heart_locations"); i < 8; ++i)
+        {
+            int64_t location_id = 0x34694200D0000 | i;
+            AP_RemoveQueuedLocationScout(state, location_id);
+        }
+
+        if (AP_GetSlotDataInt(state, "cowsanity") == 0)
+        {
+            for (int i = 0x10; i <= 0x17; ++i)
+            {
+                int64_t location_id = 0x3469420BEEF00 | i;
+                AP_RemoveQueuedLocationScout(state, location_id);
+            }
+        }
+
+        if (AP_GetSlotDataInt(state, "scrubsanity") == 0)
+        {
+            AP_RemoveQueuedLocationScout(state, 0x3469420090100 | GI_MAGIC_BEANS);
+            AP_RemoveQueuedLocationScout(state, 0x3469420090100 | GI_BOMB_BAG_40);
+            AP_RemoveQueuedLocationScout(state, 0x3469420090100 | GI_POTION_GREEN);
+            AP_RemoveQueuedLocationScout(state, 0x3469420090100 | GI_POTION_BLUE);
+        }
+
+        if (AP_GetSlotDataInt(state, "shopsanity") != 2)
+        {
+            if (AP_GetSlotDataInt(state, "shopsanity") == 1)
+            {
+                for (int i = SI_FAIRY_2; i <= SI_POTION_RED_3; ++i)
                 {
-                    for (int i = 0x00; i <= 0x1E; ++i)
-                    {
-                        if (i == 0x03)
-                        {
-                            continue;
-                        }
-                        
-                        int64_t location_id = 0x3469420062700 | i;
-                        AP_RemoveQueuedLocationScout(state, location_id);
-                    }
-                    for (int i = 0x01; i <= 0x1E; ++i)
-                    {                        
-                        int64_t location_id = 0x3469420062800 | i;
-                        AP_RemoveQueuedLocationScout(state, location_id);
-                    }
-                }
-                
-                for (int64_t i = AP_GetSlotDataInt(state, "starting_heart_locations"); i < 8; ++i)
-                {
-                    int64_t location_id = 0x34694200D0000 | i;
+                    int64_t location_id = 0x3469420090000 | i;
                     AP_RemoveQueuedLocationScout(state, location_id);
                 }
 
-                if (AP_GetSlotDataInt(state, "cowsanity") == 0)
-                {
-                    for (int i = 0x10; i <= 0x17; ++i)
-                    {                        
-                        int64_t location_id = 0x3469420BEEF00 | i;
-                        AP_RemoveQueuedLocationScout(state, location_id);
-                    }
-                }
-
-                if (AP_GetSlotDataInt(state, "scrubsanity") == 0)
-                {
-                    AP_RemoveQueuedLocationScout(state, 0x3469420090100 | GI_MAGIC_BEANS);
-                    AP_RemoveQueuedLocationScout(state, 0x3469420090100 | GI_BOMB_BAG_40);
-                    AP_RemoveQueuedLocationScout(state, 0x3469420090100 | GI_POTION_GREEN);
-                    AP_RemoveQueuedLocationScout(state, 0x3469420090100 | GI_POTION_BLUE);
-                }
-
-                if (AP_GetSlotDataInt(state, "shopsanity") != 2)
-                {
-                    if (AP_GetSlotDataInt(state, "shopsanity") == 1)
-                    {
-                        for (int i = SI_FAIRY_2; i <= SI_POTION_RED_3; ++i)
-                        {                        
-                            int64_t location_id = 0x3469420090000 | i;
-                            AP_RemoveQueuedLocationScout(state, location_id);
-                        }
-
-                        AP_RemoveQueuedLocationScout(state, 0x3469420090000 | SI_BOMB_3);
-                        AP_RemoveQueuedLocationScout(state, 0x3469420090000 | SI_ARROWS_SMALL_3);
-                        AP_RemoveQueuedLocationScout(state, 0x3469420090000 | SI_POTION_RED_6);
-                        
-                        AP_RemoveQueuedLocationScout(state, 0x346942005481E);
-                    }
-                    else
-                    {
-                        for (int i = SI_POTION_RED_1; i <= SI_POTION_RED_6; ++i)
-                        {
-                            if (i == SI_BOMB_BAG_20_1 || i == SI_BOMB_BAG_40)
-                            {
-                                continue;
-                            }
-
-                            int64_t location_id = 0x3469420090000 | i;
-                            AP_RemoveQueuedLocationScout(state, location_id);
-                        }
-
-                        AP_RemoveQueuedLocationScout(state, 0x3469420026392);
-                        AP_RemoveQueuedLocationScout(state, 0x3469420090000 | GI_CHATEAU);
-                        AP_RemoveQueuedLocationScout(state, 0x3469420006792);
-                    }
-                }
+                AP_RemoveQueuedLocationScout(state, 0x3469420090000 | SI_BOMB_3);
+                AP_RemoveQueuedLocationScout(state, 0x3469420090000 | SI_ARROWS_SMALL_3);
+                AP_RemoveQueuedLocationScout(state, 0x3469420090000 | SI_POTION_RED_6);
                 
-                AP_SendQueuedLocationScouts(state, 0);
+                AP_RemoveQueuedLocationScout(state, 0x346942005481E);
             }
-            
             else
             {
-                *((u8*) 0x0) = 0x0;  // "false" (or any other string) not supported, crash it
+                for (int i = SI_POTION_RED_1; i <= SI_POTION_RED_6; ++i)
+                {
+                    if (i == SI_BOMB_BAG_20_1 || i == SI_BOMB_BAG_40)
+                    {
+                        continue;
+                    }
+
+                    int64_t location_id = 0x3469420090000 | i;
+                    AP_RemoveQueuedLocationScout(state, location_id);
+                }
+
+                AP_RemoveQueuedLocationScout(state, 0x3469420026392);
+                AP_RemoveQueuedLocationScout(state, 0x3469420090000 | GI_CHATEAU);
+                AP_RemoveQueuedLocationScout(state, 0x3469420006792);
+                AP_RemoveQueuedLocationScout(state, 0x3469420000091);
             }
         }
         
-        else
-        {
-            *((u8*) 0x0) = 0x0;  // no apconnect.txt, crash it
-        }
+        AP_SendQueuedLocationScouts(state, 0);
+
+        _return(ctx, true);
     }
     
     DLLEXPORT void rando_skulltulas_enabled(uint8_t* rdram, recomp_context* ctx)
